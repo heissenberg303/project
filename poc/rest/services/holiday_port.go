237d98fd2fb8120a763f29988bf7c1d8df150fc2
root <root@R2D2.localdomain>
2022-11-23 23:06:55 +0700

    first commit

--- poc/rest/services/holiday_port.go
+++
@@ -1,9 +0,0 @@
-package services
-
-import "poc/model"
-
-type HolidayService interface {
-	GetAllHoliday() ([]model.HolidayResponse, error)
-	AddHoliday(model.HolidayRequest) error
-	// GetRedis(key string) (string, error)
-}
